8c4bd8ca9ce17de5a3ad969c3848efb6b0dbcfc1
febbyviolen <febbyviolen@gmail.com>
2022-05-20 03:56:47 +0900

    activity_writing update

--- app/src/main/java/com/mobileprograming/moodtracker/ui/writing/WritingActivity.kt
+++ app/src/main/java/com/mobileprograming/moodtracker/ui/writing/WritingActivity.kt
@@ -27,15 +27,12 @@ class WritingActivity : AppCompatActivity() {
         initDB()
 
         // 아래 함수를 통해 무드를 전달받습니다.
-        // var mood = intent.getIntExtra(IntentKey.MOOD_KEY,0)
+        mood = intent.getIntExtra(IntentKey.MOOD_KEY,0)
     }
-
 
     private fun initDB() {
         myDBHelper = MyDBHelper(this)
     }
-
-
 
     // 데이터베이스에 diary 추가 예제 코드일 뿐입니다.
     // 삽입 방식이 이해되셨다면 지우셔도 됩니다.
