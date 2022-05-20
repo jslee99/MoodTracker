717bab2c530071213eb3e3ce265e8ca48d3387c0
jslee99 <junsub_lee@naver.com>
2022-05-20 15:53:50 +0900

    callendar xml

--- app/src/main/java/com/mobileprograming/moodtracker/ui/calendar/CalendarActivity.kt
+++ app/src/main/java/com/mobileprograming/moodtracker/ui/calendar/CalendarActivity.kt
@@ -1,10 +1,13 @@
 package com.mobileprograming.moodtracker.ui.calendar
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.drawable.toBitmap
+import androidx.recyclerview.widget.GridLayoutManager
 import com.mobileprograming.moodtracker.R
 import com.mobileprograming.moodtracker.data.Diary
 import com.mobileprograming.moodtracker.data.MyDBHelper
@@ -13,8 +16,12 @@ import com.mobileprograming.moodtracker.ui.detail.DetailActivity
 import com.mobileprograming.moodtracker.ui.writing.WritingActivity
 import com.mobileprograming.moodtracker.util.IntentKey
 import java.lang.System.currentTimeMillis
+import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.system.measureTimeMillis
 
 class CalendarActivity : AppCompatActivity() {
@@ -22,14 +29,22 @@ class CalendarActivity : AppCompatActivity() {
     lateinit var binding: ActivityCalendarBinding
     lateinit var myDBHelper: MyDBHelper
     lateinit var diaryList : List<Diary>
+    lateinit var localDate : LocalDate
+    lateinit var adapter : CalendarViewAdapter
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCalendarBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        localDate = LocalDate.now()
         initDB()
         initList()
+
+        initRecyclerVIew()
+
+        setMonthYearTextView(localDate)
+        setRecyclerView(localDate)
     }
 
     private fun initDB() {
@@ -61,5 +76,71 @@ class CalendarActivity : AppCompatActivity() {
         val intent = Intent(this, DetailActivity::class.java)
         intent.putExtra(IntentKey.DIARY_KEY, diary)
         startActivity(intent)
+    }
+
+    private fun initRecyclerVIew(){
+        //empty adapter 연결, layoutmanager 설정
+        adapter = CalendarViewAdapter(ArrayList<Diary>())
+        binding.calendarRecylcerView.adapter = adapter
+        binding.calendarRecylcerView.layoutManager = GridLayoutManager(this, 7)
+    }
+
+    //selectedDate의 month에 맞춰서 daysInMonthArray(size : 7 * 6 = 42)를 반환, 해당하는 month에 없는 날짜면 -1을 넣는다.
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun daysInMonthArray(selectedDate : LocalDate) : ArrayList<Int>{
+        val DaysInMonthArray = ArrayList<Int>()
+        val yearMonth = YearMonth.from(selectedDate)
+
+        //현재 month의 길이
+        val daysInMonth = yearMonth.lengthOfMonth()
+        //현재 month의 첫번째날을 가지고 온후에 그 날이 일요일이면 0, 월요일이면 1,.....을 반환하여 dayOfWeek에 저장
+        val firstOfMonth = selectedDate.withDayOfMonth(1)
+        val dayOfWeek = firstOfMonth.dayOfWeek.value
+        for(i in 1..42){
+            if(i <= dayOfWeek || i > daysInMonth + dayOfWeek){
+                DaysInMonthArray.add(-1)
+            }else{
+                DaysInMonthArray.add(i + dayOfWeek)
+            }
+        }
+        return DaysInMonthArray
+    }
+
+    //year와 month textview를 selectedDate에 따라 set
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setMonthYearTextView(selectedDate : LocalDate){
+        val monthF = DateTimeFormatter.ofPattern("MM")
+        val yearF = DateTimeFormatter.ofPattern("yyyy")
+        binding.monthText.text = selectedDate.format(monthF)
+        binding.yearText.text = selectedDate.format(yearF)
+    }
+
+    //selectedDate에 따라 calendar recycler view 변경
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setRecyclerView(selectedDate: LocalDate){
+        //set adapter.DiaryListfd
+        adapter.DiaryList.clear()
+        for(i in 1..42){
+
+            val DaysInMonthArray = daysInMonthArray(selectedDate)
+            var ldate : Long
+            if(DaysInMonthArray[i] != -1){
+                val sdf = SimpleDateFormat("yyyy.MM.dd")
+                var dayStr = DaysInMonthArray[i].toString()
+                if(dayStr.length == 1){
+                    dayStr = "0" + dayStr
+                }
+                val formatStr = binding.yearText.text.toString() + "." + binding.monthText.text.toString() + "." + dayStr
+                val date = sdf.parse(formatStr)
+                ldate = date.time
+            }else{
+                ldate = -1
+            }
+            val mood = 0
+            val content = ""
+            val image = null
+            adapter.DiaryList.add(Diary(ldate, mood, content, image))
+        }
+        adapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/mobileprograming/moodtracker/ui/calendar/CalendarViewAdapter.kt
+++ app/src/main/java/com/mobileprograming/moodtracker/ui/calendar/CalendarViewAdapter.kt
@@ -1,7 +1,10 @@
 package com.mobileprograming.moodtracker.ui.calendar
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.mobileprograming.moodtracker.R
 import com.mobileprograming.moodtracker.data.Diary
@@ -10,48 +13,53 @@ import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
-class CalendarViewAdapter(var DiaryList : ArrayList<Diary>) : RecyclerView.Adapter<CalendarViewAdapter.ViewHolder>() {
+class CalendarViewAdapter(val DiaryList : ArrayList<Diary>) : RecyclerView.Adapter<CalendarViewAdapter.ViewHolder>() {
     interface onItemClickListener{
         fun onItemClick(cellData : Diary)
     }
 
     var itemClickListener : onItemClickListener? = null
 
-    inner class ViewHolder(val binding : CalendarCellBinding) : RecyclerView.ViewHolder(binding.root){
+    inner class ViewHolder(val itemView : View) : RecyclerView.ViewHolder(itemView){
+
+        val dayText = itemView.findViewById<TextView>(R.id.dayText)
+        val moodImage = itemView.findViewById<ImageView>(R.id.moodImage)
         init{
-            binding.root.setOnClickListener {
+            itemView.setOnClickListener {
                 itemClickListener?.onItemClick(DiaryList[adapterPosition])
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = CalendarCellBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.calendar_cell, parent, false)
+        view.layoutParams.height = (parent.height * 0.1666666).toInt()
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.apply {
-            //날짜 설정, 선택된 Month에 day가 없으면 date가 -1
-            if(DiaryList[position].date != (-1).toLong()) {
-                val sdf = SimpleDateFormat("dd")
-                val date = Date(DiaryList[position].date)
-                val dayString = sdf.format(date)
-                dayText.text = dayString
-            }else{
-                dayText.text = ""
-            }
-            //mood image 설정
+
+        //날짜 설정, 선택된 Month에 day가 없으면 date ==  -1
+        if(DiaryList[position].date != (-1).toLong()) {
+            val sdf = SimpleDateFormat("dd")
+            val date = Date(DiaryList[position].date)
+            val dayString = sdf.format(date)
+            holder.dayText.text = dayString
             if(DiaryList[position].mood > -1){
                 when(DiaryList[position].mood){
-                    0 -> moodImage.setImageResource(R.drawable.sohappy_0)
-                    1 -> moodImage.setImageResource(R.drawable.happy_1)
-                    2 -> moodImage.setImageResource(R.drawable.ok_2)
-                    3 -> moodImage.setImageResource(R.drawable.angry_3)
-                    4 -> moodImage.setImageResource(R.drawable.sad_4)
+                    0 -> holder.moodImage.setImageResource(R.drawable.sohappy_0)
+                    1 -> holder.moodImage.setImageResource(R.drawable.happy_1)
+                    2 -> holder.moodImage.setImageResource(R.drawable.ok_2)
+                    3 -> holder.moodImage.setImageResource(R.drawable.angry_3)
+                    4 -> holder.moodImage.setImageResource(R.drawable.sad_4)
                 }
             }
+        }else{
+            holder.dayText.text = ""
         }
+            //mood image 설정
+
+
     }
 
     override fun getItemCount(): Int {
