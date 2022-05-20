49eac71e2f3d6a40b5c12ee4a56b4a8cff714ce3
jslee99 <junsub_lee@naver.com>
2022-05-20 13:50:12 +0900

    callendar xml

--- app/src/main/java/com/mobileprograming/moodtracker/ui/calendar/CalendarViewAdapter.kt
+++ app/src/main/java/com/mobileprograming/moodtracker/ui/calendar/CalendarViewAdapter.kt
@@ -1,4 +1,60 @@
 package com.mobileprograming.moodtracker.ui.calendar
 
-class CalendarViewAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.mobileprograming.moodtracker.R
+import com.mobileprograming.moodtracker.data.Diary
+import com.mobileprograming.moodtracker.databinding.CalendarCellBinding
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+class CalendarViewAdapter(var DiaryList : ArrayList<Diary>) : RecyclerView.Adapter<CalendarViewAdapter.ViewHolder>() {
+    interface onItemClickListener{
+        fun onItemClick(cellData : Diary)
+    }
+
+    var itemClickListener : onItemClickListener? = null
+
+    inner class ViewHolder(val binding : CalendarCellBinding) : RecyclerView.ViewHolder(binding.root){
+        init{
+            binding.root.setOnClickListener {
+                itemClickListener?.onItemClick(DiaryList[adapterPosition])
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = CalendarCellBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.apply {
+            //날짜 설정, 선택된 Month에 day가 없으면 date가 -1
+            if(DiaryList[position].date != (-1).toLong()) {
+                val sdf = SimpleDateFormat("dd")
+                val date = Date(DiaryList[position].date)
+                val dayString = sdf.format(date)
+                dayText.text = dayString
+            }else{
+                dayText.text = ""
+            }
+            //mood image 설정
+            if(DiaryList[position].mood > -1){
+                when(DiaryList[position].mood){
+                    0 -> moodImage.setImageResource(R.drawable.sohappy_0)
+                    1 -> moodImage.setImageResource(R.drawable.happy_1)
+                    2 -> moodImage.setImageResource(R.drawable.ok_2)
+                    3 -> moodImage.setImageResource(R.drawable.angry_3)
+                    4 -> moodImage.setImageResource(R.drawable.sad_4)
+                }
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return DiaryList.size
+    }
 }
