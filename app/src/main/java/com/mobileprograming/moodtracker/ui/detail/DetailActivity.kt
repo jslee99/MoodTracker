823676d2025be7516c23ffca6e8bf737fe77bbd3
kyoona <yoona405@konkuk.ac.kr>
2022-05-27 10:11:28 +0900

    5.27 10:11

--- app/src/main/java/com/mobileprograming/moodtracker/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/mobileprograming/moodtracker/ui/detail/DetailActivity.kt
@@ -1,12 +1,16 @@
 package com.mobileprograming.moodtracker.ui.detail
 
+import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.drawable.toBitmap
 import com.mobileprograming.moodtracker.R
 import com.mobileprograming.moodtracker.data.Diary
+import com.mobileprograming.moodtracker.data.MyDBHelper
 import com.mobileprograming.moodtracker.databinding.ActivityDetailBinding
+import com.mobileprograming.moodtracker.ui.calendar.CalendarActivity
 import com.mobileprograming.moodtracker.util.IntentKey
 
 class DetailActivity : AppCompatActivity() {
@@ -22,11 +26,44 @@ class DetailActivity : AppCompatActivity() {
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         init()
-        // 아래 코드를 통해 diary 객체를 받아옵니다
-        // val diary = intent.getSerializableExtra(IntentKey.DIARY_KEY) as Diary
     }
     private fun init()
     {
+        val intent=intent
+        val date=intent.getLongExtra("date",0)//TODO name값 수정 필요
 
+        var diary:Diary?=null
+        val db=MyDBHelper(this)
+        val diarylist=db.getAllDiary()
+        for(getDiary in diarylist){
+            if(getDiary.date==date){ //TODO 수정 필요
+                diary=getDiary
+                break
+            }
+        }
+
+        val imageNum=diary?.mood
+        var emotionDraw: Int?=null
+        when(imageNum){
+            1 -> emotionDraw = R.drawable.happy_1
+            2 -> emotionDraw=R.drawable.ok_2
+            3 -> emotionDraw=R.drawable.angry_3
+            4 -> emotionDraw=R.drawable.sad_4
+        }
+        binding.apply {
+            if(emotionDraw!=null){
+                diaryImage.setImageResource(emotionDraw)
+            }
+            val dateString= diary?.date.toString()
+            diaryDate.text=dateString
+
+            val imagebyte=diary?.image
+            if (imagebyte != null) {
+                diaryImage.setImageBitmap(BitmapFactory.decodeByteArray(imagebyte,0,imagebyte.size))
+            }
+        }
+        binding.closeButton.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/mobileprograming/moodtracker/ui/diarylist/DiaryListActivity.kt
+++ app/src/main/java/com/mobileprograming/moodtracker/ui/diarylist/DiaryListActivity.kt
@@ -1,10 +1,12 @@
 package com.mobileprograming.moodtracker.ui.diarylist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mobileprograming.moodtracker.data.Diary
 import com.mobileprograming.moodtracker.data.MyDBHelper
 import com.mobileprograming.moodtracker.databinding.ActivityDiaryListBinding
+import com.mobileprograming.moodtracker.ui.writing.WritingActivity
 
 class DiaryListActivity : AppCompatActivity() {
     lateinit var diaryListBinding: ActivityDiaryListBinding
@@ -17,10 +19,18 @@ class DiaryListActivity : AppCompatActivity() {
     private fun init()
     {
         diaryListBinding.addButton.setOnClickListener {
-            //TODO
+            val intent= Intent(this, WritingActivity::class.java)
+            startActivity(intent)
         }
         val database=MyDBHelper(this)
         var diaryList= database.getAllDiary()
-        diaryListBinding.recyclerView.adapter=MyAdapter(diaryList)
+        val exceptNullList=ArrayList<Diary>() // content내용이 비어있고 기분만 저장되어있는 경우 -> 애초에 list에서 제외 시켜 보여주지 않음
+        val i:Int=0
+        for(diary in diaryList){
+            if(diary.content!=null){
+                exceptNullList.add(diary)
+            }
+        }
+        diaryListBinding.recyclerView.adapter=MyAdapter(exceptNullList) //TODO 나중에 일기를 쓰고 나서 데이더가 업데이트 되었을때 오늘분의 일기가 표시될지 고려해봐야됨
     }
 }
