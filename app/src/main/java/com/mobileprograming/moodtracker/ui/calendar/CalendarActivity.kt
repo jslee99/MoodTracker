75a152087bac009442d4bd73c016288fc33dd4a5
jslee99 <junsub_lee@naver.com>
2022-05-28 12:00:31 +0900

    .

--- app/src/main/java/com/mobileprograming/moodtracker/ui/calendar/CalendarActivity.kt
+++ app/src/main/java/com/mobileprograming/moodtracker/ui/calendar/CalendarActivity.kt
@@ -60,13 +60,13 @@ class CalendarActivity : AppCompatActivity() {
         // 아래 함수를 통해 데이터베이스에서 다이어리 리스트 호출 가능
         // diaryList = myDBHelper.getAllDiary()
         // 테스트용 다이어리 리스트입니다.
-        diaryList = listOf(
-            Diary(0,0,"test 0",
-                ResourcesCompat.getDrawable(resources, R.drawable.test, null)?.toBitmap()),
-            Diary(1,1,"test 1",
-                ResourcesCompat.getDrawable(resources, R.drawable.test, null)?.toBitmap()),
-            Diary(2,2,"test 2",
-                ResourcesCompat.getDrawable(resources, R.drawable.test, null)?.toBitmap()))
+//        diaryList = listOf(
+//            Diary(0,0,"test 0",
+//                ResourcesCompat.getDrawable(resources, R.drawable.test, null)?.toBitmap()),
+//            Diary(1,1,"test 1",
+//                ResourcesCompat.getDrawable(resources, R.drawable.test, null)?.toBitmap()),
+//            Diary(2,2,"test 2",
+//                ResourcesCompat.getDrawable(resources, R.drawable.test, null)?.toBitmap()))
     }
 
     private fun intentSettingActivity(){
